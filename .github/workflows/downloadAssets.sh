9f23115712d18d2572181c9f587389d8ca7d92a7
zzh <zzh-www@qq.com>
2023-04-13 23:35:10 +0800

    feature:实现git workflow

---
+++ .github/workflows/downloadAssets.sh
@@ -0,0 +1,15 @@
+filename="final_en.zip"
+fileid="1XSrU-vki1kDHxHIzXSAkL5JOjohiOQo6"
+wget --no-check-certificate "https://drive.google.com/uc?export=download&id=${fileid}" -O "app/src/main/assets/${filename}"
+filename="word_en.txt"
+fileid="11eYRc8Iqonzk19_P-zasGPHwSRJmteiK"
+wget --no-check-certificate "https://drive.google.com/uc?export=download&id=${fileid}" -O "app/src/main/assets/${filename}"
+filename="final_zh.zip"
+fileid="1QXC4DMmmFnmSvUtoeBaqHzbIkYPNEj33"
+wget --no-check-certificate "https://drive.google.com/uc?export=download&id=${fileid}" -O "app/src/main/assets/${filename}"
+filename="word_zh.txt"
+fileid="1YMUrWCeCcYE1A-3mllOVAqfiUMYNG6pp"
+wget --no-check-certificate "https://drive.google.com/uc?export=download&id=${fileid}" -O "app/src/main/assets/${filename}"
+
+
+
